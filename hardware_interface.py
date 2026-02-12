9531da7cd48d5f142f2c732604574da9c069579b
imagn002-glitch <imagn002@ucr.edu>
2026-02-12 15:44:46 -0800

    Update hardware_interface.py

--- hardware_interface.py
+++ hardware_interface.py
@@ -1,38 +1,14 @@
-"""
-Abstract hardware output layer.
-Can later support:
-- GPIO LEDs
-- Media control
-- Menu navigation
-- Serial communication
-"""
+def execute_action(gesture):
+    if gesture == "swipe_left":
+        print("Previous slide")
 
-def initialize_hardware():
-    """
-    Initialize hardware output system.
-    Leave empty during development.
-    """
-    pass
+    elif gesture == "swipe_right":
+        print("Next slide")
+
+    elif gesture == "stop":
+        print("Pause")
+
+    else:
+        pass
 
 
-def execute_output(gesture):
-    """
-    Map gesture to physical/system action.
-
-    Examples:
-    - Turn LED on/off
-    - Play/pause media
-    - Navigate menu
-    """
-
-    # if gesture == "swipe_left":
-    #     perform action
-
-    pass
-
-
-def cleanup():
-    """
-    Cleanup hardware resources.
-    """
-    pass
